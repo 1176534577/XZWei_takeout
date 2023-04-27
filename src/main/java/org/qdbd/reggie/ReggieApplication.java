bdec525a85a5db7e91ee8615c8a5037e253a92fa
RRRYJ <1176534577@qq.com>
2023-04-27 21:02:54 +0800

    添加一些的小的修改

--- src/main/java/org/qdbd/reggie/ReggieApplication.java
+++ src/main/java/org/qdbd/reggie/ReggieApplication.java
@@ -4,12 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Slf4j
 @SpringBootApplication
 @ServletComponentScan(basePackages = {"org.qdbd.reggie.filter"})
 @EnableTransactionManagement
+// 开启Spring Cache注解方式的缓存功能
+@EnableCaching
 public class ReggieApplication {
     public static void main(String[] args) throws RuntimeException {
         SpringApplication.run(ReggieApplication.class, args);
